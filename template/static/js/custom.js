7c0a0568f02a06aa2f662406926fe52f433a360e
samuelrandall <samuelrandall999@gmail.com>
2019-10-24 16:31:33 -0500

    moved script into js folder, added sitemap.xml

--- template/static/js/custom.js
+++ template/static/js/custom.js
@@ -405,6 +405,55 @@
 	$(window).on("resize", function(){
 		NAY.HeaderHeight();
 	});
+	function submitToAPI(e) {
+		e.preventDefault();
+		var URL = "https://eibajejss4.execute-api.us-east-1.amazonaws.com/default/node-email-service";
+
+				var Namere = /[A-Za-z]{1}[A-Za-z]/;
+				if (!Namere.test($("#name-input").val())) {
+										 alert ("Name can not be less than 2 char");
+						return;
+				}
+				if ($("#email-input").val()=="") {
+						alert ("Please enter your email address");
+						return;
+				}
+
+				var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+				if (!reeamil.test($("#email-input").val())) {
+						alert ("Please enter a valid email address");
+						return;
+				}
+
+		var name = $("#name-input").val();
+		var email = $("#email-input").val();
+		var desc = $("#description-input").val();
+		var data = {
+			name : name,
+			email : email,
+			desc : desc
+		};
+
+		$.ajax({
+		 type: "POST",
+		 url : "https://eibajejss4.execute-api.us-east-1.amazonaws.com/default/node-email-service",
+		 dataType: "json",
+		 crossDomain: "true",
+		 contentType: "application/json; charset=utf-8",
+		 data: JSON.stringify(data),
+
+
+		 success: function () {
+			 // clear form and show a success message
+			 alert("Successful");
+			 document.getElementById("contact-form").reset();
+		location.reload();
+		 },
+		 error: function () {
+			 // show an error message
+			 alert("UnSuccessful");
+		 }});
+}
 
 
 })(jQuery);
